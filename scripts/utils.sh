d376abd33bee43a21bfc62669a31723e38b60aee
Jhonathas Matos <jhonathas@gmail.com>
2014-10-20 19:40:38 -0200

    `repo` function

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -3,6 +3,17 @@ reload() {
   source ~/.bash_profile;
 }
 
+_repo() {
+  local cur=${COMP_WORDS[COMP_CWORD]}
+  COMPREPLY=( $(compgen -W "$(ls ~/src)" -- $cur) )
+}
+
+repo() {
+  cd ~/src/$1
+}
+
+complete -F _repo repo
+
 see() {
   qlmanage -p "$@" &>/dev/null
 }
