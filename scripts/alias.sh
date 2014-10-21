5a03d0c4d2e6d61c1cfdb71e6756647bd5cfb050
Jhonathas Matos <jhonathas@gmail.com>
2014-10-21 11:56:20 -0200

    Alias to open repos.

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -2,3 +2,8 @@ alias ll="ls -Glahs"
 alias ls="/bin/ls -G"
 alias psgrep="ps aux | egrep -v egrep | egrep"
 alias preview="open -a Preview"
+
+# alias to open repos
+for repo in $(ls ~/src); do
+  alias "r-$repo"="repo open $repo"
+done
