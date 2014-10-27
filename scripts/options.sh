851d86e53e414f2a05d8af9080d9ca672e5ac257
Jhonathas Matos <jhonathas@gmail.com>
2014-10-27 19:40:40 -0200

    Rebind `stop` to C-z and, now, I have i-search with C-s.

--- scripts/options.sh
+++ scripts/options.sh
@@ -1,3 +1,4 @@
 unset MAILCHECK                           # disable "you have mail" warning
 bind "set completion-ignore-case on"      # ignore case on bash completion
 bind "set mark-symlinked-directories on"  # add trailing slash to symlinked directories
+stty stop ^.                              # rebind freezing terminal to C-. and now C-s should work as i-search
