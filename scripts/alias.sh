ae576c0a19dca80e8a011f9362352b438ea565c1
Jhonathas Matos <jhonathas@gmail.com>
2015-10-12 13:39:40 -0300

    Change alias from Docker.

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -2,20 +2,24 @@ alias ll="ls -Glahs"
 alias ls="/bin/ls -G"
 alias psgrep="ps aux | egrep -v egrep | egrep"
 alias preview="open -a Preview"
+alias gobash="cd ~/.bash.d/"
 
 # alias to open repos
 for repo in $(ls ~/src); do
   alias "r-$repo"="repo open $repo"
 done
 
+alias dkexec='dk exec -it "${PWD##*/}_web_1"'
+alias rails="dkexec rails"
+alias rake="dkc run --rm web rake"
 alias dkc="docker-compose"
 alias dkm="docker-machine"
 alias dkbye="docker-machine stop docker"
-alias dk="docker"
 alias dkstart="docker-machine start docker"
 alias dknfs="docker-machine-nfs docker"
 alias dkbash="docker-compose run --service-ports --rm web bash"
 alias dkinit="dkstart && dknfs && reload"
+alias dkexec='dk exec -it "${PWD##*/}_web_1"'
 
-alias rails="dkc run --rm web rails"
+alias rails="dkexec rails"
 alias rake="dkc run --rm web rake"
