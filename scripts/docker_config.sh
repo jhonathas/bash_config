89c6472da60a529fd0ff3a93e874422f2c127987
Jhonathas Matos <jhonathas@gmail.com>
2016-06-22 15:17:23 -0300

    Create function to load Docker.

--- scripts/docker_config.sh
+++ scripts/docker_config.sh
@@ -1,3 +1,11 @@
+function docker-machine-init() {
+    if [ `docker-machine status develop` == "Running" ]; then
+        eval "$(docker-machine env develop)"
+    fi
+}
+
+docker-machine-init
+
 alias dk='docker'
 alias dkc="docker-compose"
 alias dkm="docker-machine"
@@ -7,19 +15,18 @@ alias dkcleaner="dk images -q --filter \"dangling=true\" | xargs docker rmi -f"
 alias dkmstart="dkm start develop"
 alias dkmstop="dkm stop develop"
 alias dkmnfs="docker-machine-nfs develop"
-alias dkminit="dkmstart && dkmnfs && eval \"$(dkm env develop)\""
+alias dkminit="dkmstart && dkmnfs && docker-machine-init"
 
 alias dkrun='dkc run --rm web'
 alias dkbash="dkrun bash"
 alias dkbashs="dkc run --service-ports --rm web bash"
-
 
 # Ruby alias to user Docker
 alias rake="dkrun bundle exec rake"
 
 function rails() {
     if [[ $@ == "s" ]]; then
-				command docker-compose run --service-ports --rm web bundle exec rails s -b 0.0.0.0
+        command docker-compose run --service-ports --rm web bundle exec rails s -b 0.0.0.0
     else
         command docker-compose run --rm web bundle exec rails "$@"
     fi
